12ac49bf051cf0889ae060ec2a98452858dfff6c
Bret Hudson <bret@brethudson.com>
2018-09-26 00:05:13 -0500

    Created initial local_settings_example.py to store a Google API Key and a Google Custom Search ID

---
+++ local_settings_example.py
@@ -0,0 +1,7 @@
+'''
+Local Settings for Cat Query Bot
+'''
+
+# Configuration for Google Console Search
+GOOGLE_API_KEY = 'Your Google API Key'
+GOOGLE_CUSTOM_SEARCH_ID = 'Your Google Custom Search Engine ID'
