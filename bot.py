3e8409526271bd5906a44ebbcd3dbc85778fbabd
Bret Hudson <bret@brethudson.com>
2018-09-26 00:54:24 -0500

    Getting total_results count and the list of images from returned JSON

--- bot.py
+++ bot.py
@@ -5,6 +5,14 @@ def generate_google_search_params(api_key, cx_id, q, safe):
 	params = { 'key': api_key, 'cx': cx_id, 'q': q, 'searchType': "image", 'safe': safe };
 	return params
 
+def get_image_links_from_json(json):
+	items = json['items']
+	image_links = []
+	for i in range(len(items)):
+		image = items[i]['image']
+		image_links.append(image['contextLink'])
+	return image_links
+
 if __name__ == "__main__":
 	# Get variables from local_settings
 	api_key = GOOGLE_API_KEY
@@ -21,6 +29,13 @@ if __name__ == "__main__":
 	
 	print(r) # Prints response code
 	if r.status_code == requests.codes.ok:
-		#print(r.text)
-		json = r.json
-		print(r.json)
+		json = r.json()
+		
+		# Get total result count
+		total_results = json['queries']['request'][0]['totalResults']
+		
+		# Get images
+		image_links = get_image_links_from_json(json)
+		print('\nFound images', image_links)
+	
+	print('\nSuccessfully finished\n')
