fcba3bd22e1dba393c6ed0ae8f75b81bb41224e5
Bret Hudson <bret@brethudson.com>
2018-09-26 00:31:20 -0500

    Now using the requests library to make a call to the Google Custom Search Engine API - generate_google_search_url now generate_google_search_params

--- bot.py
+++ bot.py
@@ -1,14 +1,23 @@
-import urllib.parse
+import requests
 from local_settings import *
 
-def generate_google_search_url(api_key, cx_id, q, safe):
+def generate_google_search_params(api_key, cx_id, q, safe):
 	params = { 'key': api_key, 'cx': cx_id, 'q': q, 'searchType': "image", 'safe': safe };
-	return "https://www.googleapis.com/customsearch/v1?" + urllib.parse.urlencode(params)
+	return params
 
 if __name__ == "__main__":
+	# Get variables from local_settings
 	api_key = GOOGLE_API_KEY
 	cx_id = GOOGLE_CUSTOM_SEARCH_ID
 	safe = "active" if SAFE_SEARCH else "off"
 	
-	url = generate_google_search_url(api_key, cx_id, "sniper cat", safe)
-	print(url)
+	# Retrieve JSON data containing images
+	params = generate_google_search_params(api_key, cx_id, "sniper cat", safe)
+	r = requests.get("https://www.googleapis.com/customsearch/v1", params=params)
+	print("Requesting data from", r.url)
+	
+	print(r) # Prints response code
+	if r.status_code == requests.codes.ok:
+		#print(r.text)
+		json = r.json
+		print(r.json)
