8a11f72c3431cc29f113121968070e49afa95b34
Bret Hudson <bret@brethudson.com>
2018-09-26 00:11:45 -0500

    Added SAFE_SEARCH boolean to local settings config

--- local_settings_example.py
+++ local_settings_example.py
@@ -3,5 +3,6 @@ Local Settings for Cat Query Bot
 '''
 
 # Configuration for Google Console Search
+SAFE_SEARCH = True
 GOOGLE_API_KEY = 'Your Google API Key'
 GOOGLE_CUSTOM_SEARCH_ID = 'Your Google Custom Search Engine ID'
