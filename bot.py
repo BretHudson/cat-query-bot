a96dea00e9c2d4d0a0bdccbdb9ff738ca137ebd7
Bret Hudson <bret@brethudson.com>
2018-09-27 00:16:35 -0500

    The bot is all set up and ready to go!

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import requests
+import twitter
 from local_settings import *
 
 def generate_google_search_params(api_key, cx_id, q, safe):
@@ -6,12 +7,36 @@ def generate_google_search_params(api_key, cx_id, q, safe):
 	return params
 
 def get_image_links_from_json(json):
-	items = json['items']
-	image_links = []
-	for i in range(len(items)):
-		image = items[i]['image']
-		image_links.append(image['contextLink'])
-	return image_links
+	try:
+		items = json['items']
+		image_links = []
+		for i in range(len(items)):
+			image = items[i]['link']
+			image_links.append(image)
+		return image_links
+	except KeyError:
+		return []
+
+# Borrowed from Tom Meagher's heroku_ebooks (https://github.com/tommeagher/heroku_ebooks)
+def connect():
+	return twitter.Api(consumer_key=MY_CONSUMER_KEY,
+                       consumer_secret=MY_CONSUMER_SECRET,
+                       access_token_key=MY_ACCESS_TOKEN_KEY,
+                       access_token_secret=MY_ACCESS_TOKEN_SECRET,
+                       tweet_mode='extended')
+
+def post_tweets(word, images, total_results):
+	api = connect()
+	query_str = "\"" + word + " cat\" found "
+	results_str = "  (total wesults: " + total_results + ")"
+	success_text = "and pet sum cats ^.^"
+	fail_text = "no cats v.v"
+	str = query_str + (success_text if total_results != "0" else fail_text) + results_str
+	if Debug:
+		print("\nTweet text would be: ", str)
+	else:
+		status = api.PostUpdate(str, media=images[:4])
+		print('\n', status)
 
 if __name__ == "__main__":
 	# Get variables from local_settings
@@ -19,23 +44,36 @@ if __name__ == "__main__":
 	cx_id = GOOGLE_CUSTOM_SEARCH_ID
 	safe = "active" if SAFE_SEARCH else "off"
 	
-	# Retrieve JSON data containing images
-	params = generate_google_search_params(api_key, cx_id, "sniper cat", safe)
-	if TEST_SCRAPE_URL:
-		r = requests.get(TEST_SCRAPE_URL)
-	else:
-		r = requests.get("https://www.googleapis.com/customsearch/v1", params=params)
-	print("Requesting data from", r.url)
+	# Get latest word
+	with open('mywords.txt', 'r') as fin:
+	    data = fin.read().splitlines(True)
 	
-	print(r) # Prints response code
-	if r.status_code == requests.codes.ok:
-		json = r.json()
+	try:
+		word = data[0].rstrip()
+		params = generate_google_search_params(api_key, cx_id, "\"" + word + "\" \"cat\"", safe)
+		if TEST_SCRAPE_URL:
+			r = requests.get(TEST_SCRAPE_URL)
+		else:
+			r = requests.get("https://www.googleapis.com/customsearch/v1", params=params)
+		print("Requesting data from", r.url)
 		
-		# Get total result count
-		total_results = json['queries']['request'][0]['totalResults']
+		print(r) # Prints response code
+		if r.status_code == requests.codes.ok:
+			json = r.json()
+			
+			# Get total result count
+			total_results = json['queries']['request'][0]['totalResults']
+			
+			# Get images
+			image_links = get_image_links_from_json(json)
+			print('\nFound images', image_links)
+			
+			post_tweets(word, image_links, total_results)
+			
+			# Remove the word from our text file
+			with open('testwords.txt', 'w') as fout:
+			    fout.writelines(data[1:])
 		
-		# Get images
-		image_links = get_image_links_from_json(json)
-		print('\nFound images', image_links)
-	
-	print('\nSuccessfully finished\n')
+		print('\nSuccessfully finished\n')
+	except IndexError:
+		print('\nFailed: Words list is empty\n')
--- local_settings_example.py
+++ local_settings_example.py
@@ -7,4 +7,13 @@ SAFE_SEARCH = True
 GOOGLE_API_KEY = 'Your Google API Key'
 GOOGLE_CUSTOM_SEARCH_ID = 'Your Google Custom Search Engine ID'
 
+# Configuration for Twitter API
+MY_CONSUMER_KEY = 'Your Twitter API Consumer Key'
+MY_CONSUMER_SECRET = 'Your Consumer Secret Key'
+MY_ACCESS_TOKEN_KEY = 'Your Twitter API Access Token Key'
+MY_ACCESS_TOKEN_SECRET = 'Your Access Token Secret'
+
+WORDS_FILE = "mywords.txt" # The word list to use. Each word should be on its own line
 TEST_SCRAPE_URL = False # The url to JSON data containing image results
+
+DEBUG = True # If True, everything will run as normal but the results will not be posted to Twitter
